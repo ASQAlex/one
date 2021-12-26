50652d67bdccadfa1ee84fad348b192c08211afd
ASQAlex <72416314+ASQAlex@users.noreply.github.com>
2021-12-26 21:18:43 +0100

    Same changes

--- js/min.js
+++ js/min.js
@@ -1,22 +1,50 @@
-$(function(){
+$(function () {
 
-    $('.roll').on('click', function(e) {
-        e.preventDefault();
-
-        $($(this).siblings()).removeClass('roll--active');
-        $($(this).closest('.price__all').siblings().find('li' , 'p')).removeClass('tabs-content--active');
-
-        $(this).addClass('roll--active');
-        $($(this).attr('href')).addClass('tabs-content--active');
-    });
-
-    $('.slider__wrapper').slick({
-  dots: true,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 10,
-  centerMode: true,
-  variableWidth: true
+const burger = document.querySelector('.burger')
+burger.addEventListener('click', () => {
+burger.classList.toggle('burger--active');
 });
 
+ $('.burger').on('click', function () {
+    $('.menu__list-link').toggleClass('menu__list-link--active')
 });
+
+$('.open__popup').click(function(e){
+    e.preventDefault();
+    $('.popup__bg').fadeIn(600);
+  })
+  
+  $('.close__popup').click(function(){
+    $('.popup__bg').fadeOut(600);
+  })
+});
+
+$(function () {
+
+  $('.roll').on('click', function (e) {
+    e.preventDefault();
+
+    $($(this).siblings()).removeClass('roll--active');
+    $($(this).closest('.price').siblings().find('li')).addClass('tabs-content--active');
+
+    $(this).addClass('roll--active');
+    $($(this).attr('href')).addleClass('tabs-content--active');
+    
+  });
+
+  $('.slider__wrapper').slick({
+    dots: false,
+    arrow: true,
+    autoplay: 500,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 10,
+    centerMode: true,
+    variableWidth: true,
+  });
+});
+
+
+
+
+
