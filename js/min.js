e43ed4f74d66cef5586ea7de0f5f2abbb729c90f
ASQAlex <72416314+ASQAlex@users.noreply.github.com>
2022-04-24 15:47:27 +0200

    one

--- js/min.js
+++ js/min.js
@@ -1,50 +1,42 @@
-$(function () {
+$(function() {
+    const burger = document.querySelector('.burger')
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('burger--active');
+    });
 
-const burger = document.querySelector('.burger')
-burger.addEventListener('click', () => {
-burger.classList.toggle('burger--active');
+    $('.burger').on('click', function() {
+        $('.menu__list-link').toggleClass('menu__list-link--active')
+    });
+
+    $('.open__popup').click(function(e) {
+        e.preventDefault();
+        $('.popup__bg').fadeIn(600);
+    })
+
+    $('.close__popup').click(function() {
+        $('.popup__bg').fadeOut(600);
+    })
 });
 
- $('.burger').on('click', function () {
-    $('.menu__list-link').toggleClass('menu__list-link--active')
-});
-
-$('.open__popup').click(function(e){
-    e.preventDefault();
-    $('.popup__bg').fadeIn(600);
-  })
-  
-  $('.close__popup').click(function(){
-    $('.popup__bg').fadeOut(600);
-  })
-});
-
-$(function () {
-
-  $('.roll').on('click', function (e) {
+$('.tab').on('click', function(e) {
     e.preventDefault();
 
-    $($(this).siblings()).removeClass('roll--active');
-    $($(this).closest('.price').siblings().find('li')).addClass('tabs-content--active');
+    $($(this).siblings()).removeClass('tab--active');
+    $($(this).closest('.price__roll').siblings().find('div')).removeClass('tabs-content--active');
 
-    $(this).addClass('roll--active');
-    $($(this).attr('href')).addleClass('tabs-content--active');
-    
-  });
-
-  $('.slider__wrapper').slick({
-    dots: false,
-    arrow: true,
-    autoplay: 500,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 10,
-    centerMode: true,
-    variableWidth: true,
-  });
+    $(this).addClass('tab--active');
+    $($(this).attr('href')).addClass('tabs-content--active');
 });
 
-
-
-
-
+$(function() {
+    $('.slider__wrapper').slick({
+        dots: false,
+        arrow: true,
+        autoplay: 500,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 10,
+        centerMode: true,
+        variableWidth: true,
+    });
+});
